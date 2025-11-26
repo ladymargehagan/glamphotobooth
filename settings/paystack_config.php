564ced8816e4ceb0db5197ee982e1bbb98718192
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2025-11-26 00:25:45 +0000

    paystack secret key addition

--- settings/paystack_config.php
+++ settings/paystack_config.php
@@ -6,7 +6,7 @@
 
 // Paystack API Keys - Replace with your actual keys
 define('PAYSTACK_PUBLIC_KEY', 'pk_test_cd389d93978a547260b8e8362def282f0b015eb6');
-define('PAYSTACK_SECRET_KEY', ''); // You'll provide this
+define('PAYSTACK_SECRET_KEY', 'sk_test_4efe5499a5e58902d1d413ecbda42cfa49161943'); // You'll provide this
 
 // Paystack API URLs
 define('PAYSTACK_INITIALIZE_URL', 'https://api.paystack.co/transaction/initialize');
